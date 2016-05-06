eb1d9242ce29b41685d21b6654f69bfa3aced36c
douglas <Doug4less@gmail.com>
2016-05-06 13:57:55 -0700

    Feature: Added the library fragment.

--- app/src/main/java/com/brainbeats/LibraryActivity.java
+++ app/src/main/java/com/brainbeats/LibraryActivity.java
@@ -1,5 +1,7 @@
 package com.brainbeats;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,9 +9,15 @@ import architecture.BaseActivity;
 
 public class LibraryActivity extends BaseActivity {
 
+    Fragment mLibraryFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_base);
+    }
+
+    public void switchToLibraryFragment(){
+        //replaceFragment(mLibraryFragment, mDashboardFragment.getTag());
     }
 }
