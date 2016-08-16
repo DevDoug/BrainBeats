12db6a26d88bb37891b6459f05035dd7a81000e2
douglas <douglas@odysseyinc.com>
2016-08-15 19:48:27 -0700

    UI: layout improvments for elevation and fix scroll view

--- app/src/androidTest/java/com/brainbeats/LoginActivityTest.java
+++ app/src/androidTest/java/com/brainbeats/LoginActivityTest.java
@@ -1,43 +1,15 @@
 package com.brainbeats;
 
-import android.app.Application;
 import android.content.Context;
-
-
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.runners.MockitoJUnitRunner;
-
 import utils.Constants;
-import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.Espresso.pressBack;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static android.support.test.espresso.action.ViewActions.swipeUp;
-import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.scrollTo;
-import static android.support.test.espresso.action.ViewActions.doubleClick;
-import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.instanceOf;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.not;
-import static org.hamcrest.Matchers.anything;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasEntry;
-import static org.junit.Assert.fail;
 
 
 /**
@@ -52,6 +24,11 @@ public class LoginActivityTest {
 
     public LoginActivityTest() {
         MockitoAnnotations.initMocks(LoginActivityTest.class);
+        try {
+            testAttemptSoundCloudLogin();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @org.junit.Before
