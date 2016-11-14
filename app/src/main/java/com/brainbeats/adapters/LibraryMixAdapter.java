b8f7e70c5aa80df83c3209475159b55529f55c98
douglas <douglas@odysseyinc.com>
2016-11-14 12:19:11 -0800

    Clean-Up: Add function to delete a record from library

--- app/src/main/java/com/brainbeats/adapters/LibraryMixAdapter.java
+++ app/src/main/java/com/brainbeats/adapters/LibraryMixAdapter.java
@@ -1,15 +1,18 @@
 package com.brainbeats.adapters;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.brainbeats.LibraryActivity;
 import com.brainbeats.MainActivity;
@@ -18,6 +21,7 @@ import com.brainbeats.R;
 
 import com.brainbeats.architecture.AccountManager;
 import com.brainbeats.data.BrainBeatsContract;
+import com.brainbeats.data.BrainBeatsDbHelper;
 import com.brainbeats.model.Mix;
 import com.brainbeats.utils.Constants;
 import com.brainbeats.sync.OfflineSyncManager;
@@ -39,6 +43,21 @@ public class LibraryMixAdapter extends RecyclerViewCursorAdapter<LibraryMixAdapt
         String title = cursor.getString(cursor.getColumnIndex(BrainBeatsContract.MixEntry.COLUMN_NAME_MIX_TITLE));
         if(title != null)
             viewHolder.mTitleText.setText(title);
+
+        viewHolder.mContainer.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                //load delete dialog
+                Constants.buildActionDialog(mAdapterContext, "Delete Song", "Do you want to delete this song from your library", "confirm", new Constants.ConfirmDialogActionListener() {
+                    @Override
+                    public void PerformDialogAction() {
+                        Mix selectedMix = Constants.buildMixFromCursor(mAdapterContext,getCursor(), viewHolder.getAdapterPosition()); // get the selected mix item
+                        mAdapterContext.getContentResolver().delete(BrainBeatsContract.MixEntry.CONTENT_URI, "_Id" + BrainBeatsDbHelper.WHERE_CLAUSE_EQUAL, new String[]{String.valueOf(selectedMix.getMixId())});
+                    }
+                });
+                return false;
+            }
+        });
 
         viewHolder.mPlay.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -84,12 +103,14 @@ public class LibraryMixAdapter extends RecyclerViewCursorAdapter<LibraryMixAdapt
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
+        CardView mContainer;
         TextView mTitleText;
         ImageView mPlay;
         ImageView mMix;
 
         public ViewHolder(View view){
             super(view);
+            mContainer = (CardView) view.findViewById(R.id.card_view);
             mTitleText = (TextView) view.findViewById(R.id.album_title);
             mPlay = (ImageView) view.findViewById(R.id.play_mix);
             mMix = (ImageView) view.findViewById(R.id.mix_beat);
