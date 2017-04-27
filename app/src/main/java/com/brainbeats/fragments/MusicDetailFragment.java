0ccf18004a146191c7f5aa3f0626b32ae11bd1d1
IPSGROUPINC\douglas.ray <douglas.ray@ipsgroup.com>
2017-04-27 15:39:43 -0700

    Play new song when hitting play and not current song.

--- app/src/main/java/com/brainbeats/fragments/MusicDetailFragment.java
+++ app/src/main/java/com/brainbeats/fragments/MusicDetailFragment.java
@@ -440,7 +440,8 @@ public class MusicDetailFragment extends Fragment implements LoaderManager.Loade
     public void playSong() {
         MainActivity main = ((MainActivity) getActivity());
 
-        if (!main.mAudioService.getIsPlaying() && !main.mAudioService.getIsPaused()) {
+        if (!main.mAudioService.getIsPlaying() && !main.mAudioService.getIsPaused()
+                || !(main.mAudioService.getPlayingSong().getID() == mSelectedTrack.getID())) { //if music not playing or not the current playing song
             showLoadingMusicDialog();
             mListener.onFragmentInteraction(Constants.DASHBOARD_DETAIL_LOAD_SONG_URI, mSelectedTrack);
         } else if (main.mAudioService.getIsPlaying()) {                                                        //Song is playing so pause song
