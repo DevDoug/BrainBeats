d0be2638ef0aaa744f453770e87f849203db45b9
IPSGROUPINC\douglas.ray <douglas.ray@ipsgroup.com>
2017-08-21 16:17:05 -0700

    Add delete playlist Add users song to dashboard Add password reset

--- app/src/main/java/com/brainbeats/adapters/LibraryPlaylistAdapter.java
+++ app/src/main/java/com/brainbeats/adapters/LibraryPlaylistAdapter.java
@@ -9,10 +9,22 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.brainbeats.LibraryActivity;
 import com.brainbeats.R;
+import com.brainbeats.entity.Track;
+import com.brainbeats.model.Mix;
 import com.brainbeats.model.Playlist;
+import com.brainbeats.utils.Constants;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 /**
  * Created by douglas on 7/28/2016.
@@ -52,6 +64,47 @@ public class LibraryPlaylistAdapter extends RecyclerView.Adapter<LibraryPlaylist
     public void onBindViewHolder(LibraryPlaylistAdapter.ViewHolder holder, int position) {
         Playlist playlist = mPlaylist.get(position);
         holder.mTitleText.setText(playlist.getPlaylistTitle());
+
+        holder.mContainer.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Constants.buildActionDialog(mAdapterContext, "Delete Playlist", "Do you want to delete this playlist from your library", "Confirm", new Constants.ConfirmDialogActionListener() {
+                    @Override
+                    public void PerformDialogAction() {
+                        FirebaseDatabase Database = FirebaseDatabase.getInstance();
+                        Query reference = Database
+                                .getReference("playlists/" + FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                .orderByChild("artistId")
+                                .equalTo(FirebaseAuth.getInstance().getCurrentUser().getUid());
+
+
+                        reference.getRef().addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(DataSnapshot dataSnapshot) {
+                                for (DataSnapshot childSnapshot : dataSnapshot.getChildren()) {
+                                    Playlist playlistToDelete = childSnapshot.getValue(Playlist.class);
+                                    if(playlistToDelete.getPlaylistTitle().equals(playlistToDelete.getPlaylistTitle()))
+                                        childSnapshot.getRef().removeValue();
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(DatabaseError databaseError) {}
+                        });
+                    }
+                });
+                return false;
+            }
+        });
+
+        holder.mPlay.setOnClickListener(v -> {
+            Queue<Track> playListQue = new PriorityQueue<>();
+            for (int i = 0; i < playlist.getMixes().size(); i++) {
+                Track playListTrack = new Track(playlist.getMixes().get(i));
+                playListQue.add(playListTrack);
+            }
+            ((LibraryActivity) mAdapterContext).mAudioService.setPlaylist(playListQue);
+        });
     }
 
     @Override
